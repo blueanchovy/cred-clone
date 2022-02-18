d660f7aa8f58049d5a50dee2ca5b540e1439296f
Manish Jha <manishjhazyx@gmail.com>
2022-02-18 14:18:14 +0530

    completed feelSpecial

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/common/Header";
+import FeelSpecial from "../components/FeelSpecial";
 import HeroSection from "../components/HeroSection";
 import ProductShowCase from "../components/ProductShowcase";
 
@@ -9,6 +10,7 @@ const Homepage = () => {
       <Header />
       <HeroSection />
       <ProductShowCase />
+      <FeelSpecial />
     </>
   );
 };
